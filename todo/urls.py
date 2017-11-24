f6985a4ba1d3de9758162e436a068e3c257fa18d
Pieter Marsman <pietermarsman@gmail.com>
2017-11-24 23:42:02 +0100

    fix #4, closing and opening todo's

--- todo/urls.py
+++ todo/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from views import TodoListView, TodoDetailView
+from views import TodoListView, TodoDetailView, flip_done
 
 urlpatterns = [
     url(r'^$', TodoListView.as_view(), name='todo-list-view'),
-    url(r'^(?P<pk>[0-9]+)/$', TodoDetailView.as_view(), name='todo-detail-view')
+    url(r'^(?P<pk>[0-9]+)/$', TodoDetailView.as_view(), name='todo-detail-view'),
+    url(r'^(?P<pk>[0-9]+)/flip_done$', flip_done, name='todo-flip-done')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,8 @@
+
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.utils import timezone
 from django.views.generic import DetailView
 from django.views.generic import ListView
 
@@ -10,3 +15,14 @@ class TodoListView(ListView):
 
 class TodoDetailView(DetailView):
     model = Todo
+
+
+def flip_done(request, pk):
+    todo = Todo.objects.get(id=pk)
+    if todo.done:
+        todo.finished = None
+    else:
+        todo.finished = timezone.now()
+    todo.save()
+
+    return redirect(reverse('todo-list-view'))
