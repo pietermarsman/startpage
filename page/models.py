eca42af4122b21324c9d38bb8a4addd4eeb1af42
Pieter Marsman <pietermarsman@gmail.com>
2017-12-24 17:15:44 +0100

    fix #33; created page model

--- page/models.py
+++ page/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Label(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+
+class Page(models.Model):
+    url = models.URLField()
+    name = models.CharField(max_length=256)
+    description = models.TextField(default=None, null=True)
+    label = models.ManyToManyField(Label)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '%s (%s)' % (self.name, self.url)
--- startpage/settings.py
+++ startpage/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'todo',
     'bookmark',
+    'page',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
